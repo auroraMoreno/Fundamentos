5c8b30a040043f6b2b9706061d63256c7e6ca6da
Aurora Moreno Lopez <aurora.moreno.lopez@tajamar365.com>
2020-11-30 14:24:16 +0100

    Creada clase persona + form 26

---
+++ Form26PruebaDeClases.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Fundamentos
+{
+    partial class Form26PruebaDeClases
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDatos = new System.Windows.Forms.ListBox();
+            this.btnCrearPersona = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstDatos
+            // 
+            this.lstDatos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lstDatos.FormattingEnabled = true;
+            this.lstDatos.Location = new System.Drawing.Point(36, 45);
+            this.lstDatos.Name = "lstDatos";
+            this.lstDatos.Size = new System.Drawing.Size(414, 186);
+            this.lstDatos.TabIndex = 0;
+            // 
+            // btnCrearPersona
+            // 
+            this.btnCrearPersona.Location = new System.Drawing.Point(204, 247);
+            this.btnCrearPersona.Name = "btnCrearPersona";
+            this.btnCrearPersona.Size = new System.Drawing.Size(75, 37);
+            this.btnCrearPersona.TabIndex = 1;
+            this.btnCrearPersona.Text = "Crear Persona";
+            this.btnCrearPersona.UseVisualStyleBackColor = true;
+            this.btnCrearPersona.Click += new System.EventHandler(this.btnCrearPersona_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Datos";
+            // 
+            // Form26PruebaDeClases
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 340);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCrearPersona);
+            this.Controls.Add(this.lstDatos);
+            this.Name = "Form26PruebaDeClases";
+            this.Text = "Form26PruebaDeClases";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDatos;
+        private System.Windows.Forms.Button btnCrearPersona;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form26PruebaDeClases.cs
@@ -0,0 +1,31 @@
+﻿using ProyectoClase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form26PruebaDeClases : Form
+    {
+        public Form26PruebaDeClases()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearPersona_Click(object sender, EventArgs e)
+        {
+            //toda clase debemos instanciarla
+            Persona person = new Persona(); //no podemos hacer esto sin namespace, agregar a este proyecto las refs del otro
+            person.Nombre = "Adrian"; //dentro del propio set hace un set
+            person.Edad = -13;
+            this.lstDatos.Items.Add("Nombre: " + person.Nombre);
+            this.lstDatos.Items.Add("Edad: " + person.Edad); //13:56
+        }
+    }
+}
