1b69800667e04260a59bcfe6cd5294cc8ddab9fa
Aurora Moreno Lopez <aurora.moreno.lopez@tajamar365.com>
2020-12-02 14:18:55 +0100

    diseño serialización hecha

---
+++ Form30SerializarClase.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Fundamentos
+{
+    partial class Form30SerializarClase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.labek2 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnLeerProducto = new System.Windows.Forms.Button();
+            this.btnGuardarProducto = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Producto";
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(55, 73);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtProducto.TabIndex = 1;
+            // 
+            // labek2
+            // 
+            this.labek2.AutoSize = true;
+            this.labek2.Location = new System.Drawing.Point(52, 140);
+            this.labek2.Name = "labek2";
+            this.labek2.Size = new System.Drawing.Size(37, 13);
+            this.labek2.TabIndex = 2;
+            this.labek2.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(55, 169);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // btnLeerProducto
+            // 
+            this.btnLeerProducto.Location = new System.Drawing.Point(55, 213);
+            this.btnLeerProducto.Name = "btnLeerProducto";
+            this.btnLeerProducto.Size = new System.Drawing.Size(75, 41);
+            this.btnLeerProducto.TabIndex = 4;
+            this.btnLeerProducto.Text = "Leer Producto";
+            this.btnLeerProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardarProducto
+            // 
+            this.btnGuardarProducto.Location = new System.Drawing.Point(158, 213);
+            this.btnGuardarProducto.Name = "btnGuardarProducto";
+            this.btnGuardarProducto.Size = new System.Drawing.Size(75, 41);
+            this.btnGuardarProducto.TabIndex = 5;
+            this.btnGuardarProducto.Text = "Guardar Producto";
+            this.btnGuardarProducto.UseVisualStyleBackColor = true;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(55, 268);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(34, 13);
+            this.lblInfo.TabIndex = 6;
+            this.lblInfo.Text = "lblinfo";
+            // 
+            // Form30SerializarClase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 335);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnGuardarProducto);
+            this.Controls.Add(this.btnLeerProducto);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.labek2);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form30SerializarClase";
+            this.Text = "Form30SerializarClase";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.Label labek2;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnLeerProducto;
+        private System.Windows.Forms.Button btnGuardarProducto;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Fundamentos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form29FicheroProductos());
+            Application.Run(new Form30SerializarClase());
         }
     }
 }
